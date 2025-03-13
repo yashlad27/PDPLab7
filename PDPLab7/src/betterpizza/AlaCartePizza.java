390212d383e33c89fbc89b10445f896c0cda03b9
Yash Lad <lad.yas@northeastern.edu>
2025-03-13 16:14:08 -0400

    added Builder class as a inner class in Alacarte pizza

--- PDPLab7/src/betterpizza/AlaCartePizza.java
+++ PDPLab7/src/betterpizza/AlaCartePizza.java
@@ -43,6 +43,45 @@ public class AlaCartePizza implements ObservablePizza {
     this.toppings = new HashMap<>(toppings);
   }
 
+  public static class AlaCartePizzaBuilder extends PizzaBuilder {
+
+    private Size size;
+    private Crust crust;
+    private Map<ToppingName, ToppingPortion> toppings;
+
+    /**
+     * Create a new AlacartePizzaBuilder.
+     */
+    public AlaCartePizzaBuilder() {
+      this.toppings = new HashMap<>();
+    }
+
+    @Override
+    public PizzaBuilder crust(Crust crust) {
+      this.crust = crust;
+      return this;
+    }
+
+    @Override
+    public PizzaBuilder size(Size size) {
+      this.size=size;
+      return this;
+    }
+
+    private AlaCartePizzaBuilder addTopping(ToppingName name, ToppingPortion portion) {
+      this.toppings.put(name, portion);
+      return this;
+    }
+
+    @Override
+    public ObservablePizza build() {
+      if(size == null){
+        throw new IllegalStateException("Size must be specified");
+      }
+      return new AlaCartePizza(size, crust != null ? crust : Crust.Classic, toppings);
+    }
+  }
+
   /**
    * Get the cost of this pizza.
    *
