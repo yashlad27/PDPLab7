82bfa5344f2f8dc22962504f4444bc11d80db0db
Yash Lad <lad.yas@northeastern.edu>
2025-03-13 16:13:39 -0400

    added documentation for abstract methods

--- PDPLab7/src/betterpizza/PizzaBuilder.java
+++ PDPLab7/src/betterpizza/PizzaBuilder.java
@@ -7,9 +7,28 @@ import pizza.Size;
  * Abstract class for building pizzas.
  */
 public abstract class PizzaBuilder {
+
+  /**
+   * Set the crust for the pizza being built.
+   *
+   * @param crust crust to set
+   * @return this builder
+   */
   public abstract PizzaBuilder crust(Crust crust);
 
+  /**
+   * Set the size for the pizza being built.
+   *
+   * @param size the size to set
+   * @return this builder
+   */
   public abstract PizzaBuilder size(Size size);
 
+  /**
+   * Build the pizza with current configuration.
+   *
+   * @return the built pizza
+   * @throws IllegalStateException if the pizza configuration is invalid
+   */
   public abstract ObservablePizza build();
 }
