6f334cfcc2bedbe7cf3c3068036e343c0ac896d0
Yash Lad <lad.yas@northeastern.edu>
2025-03-13 15:44:57 -0400

    modified AlacartePizza to implement ObservablePizza and its methods

--- PDPLab7/src/betterpizza/AlaCartePizza.java
+++ PDPLab7/src/betterpizza/AlaCartePizza.java
@@ -1,9 +1,31 @@
 package betterpizza;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import pizza.Crust;
+import pizza.Size;
 import pizza.ToppingName;
 import pizza.ToppingPortion;
 
-public class AlaCartePizza implements ObservablePizza{
+/**
+ * This class represents a alacarte pizza ( x number of ingredients ).
+ */
+public class AlaCartePizza implements ObservablePizza {
+
+  protected Crust crust;
+  protected Size size;
+  protected Map<ToppingName, ToppingPortion> toppings;
+
+  /**
+   * Create a pizza given its crust type, size and toppings.
+   */
+  public AlaCartePizza(Size size, Crust crush) {
+    this.crust = crust;
+    this.size = size;
+    this.toppings = new HashMap<ToppingName, ToppingPortion>();
+  }
+
   /**
    * Get the cost of this pizza.
    *
@@ -11,7 +33,11 @@ public class AlaCartePizza implements ObservablePizza{
    */
   @Override
   public double cost() {
-    return 0;
+    double cost = 0.0;
+    for(Map.Entry<ToppingName, ToppingPortion> item : this.toppings.entrySet()) {
+      cost += item.getKey().getCost() * item.getValue().getCostMultiplier();
+    }
+    return cost + this.size.getBaseCost();
   }
 
   /**
@@ -22,6 +48,6 @@ public class AlaCartePizza implements ObservablePizza{
    */
   @Override
   public ToppingPortion hasTopping(ToppingName name) {
-    return null;
+    return this.toppings.getDefault(name, null);
   }
 }
